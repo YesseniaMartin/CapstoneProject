9465dd80a9856c33bbd628538b8cb1c93e4a3a89
yessenia <yesseniaperez.n5@gmail.com>
2024-12-14 08:36:42 -0700

    EntityScan jpaonlineshoestore

--- OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/OnlineShoeStoreApplication.java
+++ OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/OnlineShoeStoreApplication.java
@@ -2,7 +2,9 @@ package com.skilldistillery.onlineshoestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.skilldistillery.jpaonlineshoestore")
 @SpringBootApplication
 public class OnlineShoeStoreApplication {
 
