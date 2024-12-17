25f140b54220c00d8457d217a4694c2ed2113d79
sazuko07 <sazu07@gmail.com>
2024-12-17 14:10:10 -0600

    userDAOImpl completed code blocks

--- OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/UserDAOImpl.java
+++ OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/UserDAOImpl.java
@@ -1,8 +1,7 @@
 package com.skilldistillery.onlineshoestore.data;
 
+import com.skilldistillery.jpaonlineshoestore.entities.*;
 import org.springframework.stereotype.Service;
-
-import com.skilldistillery.jpaonlineshoestore.entities.User;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -11,31 +10,71 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class UserDAOImpl implements UserDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
+	User updateuser = new User();
+	User newUser = new User();
 
 	@Override
 	public User addUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(user);
+		newUser = em.find(User.class, user.getId());
+		return user;
 	}
 
 	@Override
-	public User deleteUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean deleteUser(User user) {
+		boolean wasDeleted = false;
+
+		User userToBeDeleted = em.find(User.class, user);
+		if (userToBeDeleted != null)
+			em.remove(userToBeDeleted);
+		wasDeleted = true;
+
+		wasDeleted = !em.contains(userToBeDeleted);
+		return wasDeleted;
 	}
 
-	@Override
+	@Override /*make sure to test */
 	public User updateUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
+		updateuser = null;
+		String jpql = "UPDATE user user " + "SET user.id = :id " + "SET user.firstname = :firstname "
+				+ "SET user.lastname = :lastname " + "SET user.email = :email" + "SET user.phone = :phone "
+				+ "SET user.user = :user ";
+
+		User updatedUser = em.find(User.class, user.getId());
+
+		if (user.getUsername() == null) {
+			user.setUsername(updatedUser.getUsername());
+		}
+		if (user.getPassword() == null) {
+			user.setPassword(updatedUser.getPassword());
+		}
+		if (user.getEnabled() == false) {
+			user.setEnabled(true);
+		}
+		if (user.getRole() == null) {
+			user.setRole(updatedUser.getRole());
+		}
+		
+		int updateColumns = em.createQuery(jpql).setParameter("username", user.getUsername())
+				.setParameter("passwrd", user.getPassword()).setParameter("enabled", user.getEnabled())
+				.setParameter("role", user.getRole()).executeUpdate();
+
+		if (updateColumns > 0) {
+			return updatedUser;
+		} else {
+
+			return updateuser;
+		}
 	}
 
 	@Override
 	public User findUserById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = em.find(User.class, id);
+		
+		return user;
 	}
+
 }
