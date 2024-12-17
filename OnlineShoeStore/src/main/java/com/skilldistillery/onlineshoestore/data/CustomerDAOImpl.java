f68dddd4d3a8e8666aec8213d4b456fd7fe86361
sazuko07 <sazu07@gmail.com>
2024-12-17 13:17:51 -0600

    customerDAOImpl completed code blocks

--- OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/CustomerDAOImpl.java
+++ OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/CustomerDAOImpl.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.onlineshoestore.data;
 
+import com.skilldistillery.jpaonlineshoestore.entities.*;
 import org.springframework.stereotype.Service;
 
 import com.skilldistillery.jpaonlineshoestore.entities.Customer;
@@ -11,33 +12,74 @@ import jakarta.transaction.Transactional;
 @Service
 @Transactional
 public class CustomerDAOImpl implements CustomerDAO {
-	
+
 	@PersistenceContext
 	private EntityManager em;
+	Customer updateCustomer = new Customer();
+	Customer newCustomer = new Customer();
 
 	@Override
 	public Customer addCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(customer);
+		newCustomer = em.find(Customer.class, customer.getId());
+		return customer;
 	}
 
 	@Override
-	public Customer deleteCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+	public boolean deleteCustomer(Customer customer) {
+		boolean wasDeleted = false;
+
+		Customer customerToBeDeleted = em.find(Customer.class, customer);
+		if (customerToBeDeleted != null)
+			em.remove(customerToBeDeleted);
+		wasDeleted = true;
+
+		wasDeleted = !em.contains(customerToBeDeleted);
+		return wasDeleted;
 	}
 
-	@Override
+	@Override /*make sure to test */
 	public Customer updateCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+		updateCustomer = null;
+		String jpql = "UPDATE Customer customer " + "SET Customer.id = :id " + "SET Customer.firstname = :firstname "
+				+ "SET Customer.lastname = :lastname " + "SET Customer.email = :email" + "SET Customer.phone = :phone "
+				+ "SET Customer.user = :user ";
+
+		Customer updatedCustomer = em.find(Customer.class, customer.getId());
+
+		if (customer.getFirstName() == null) {
+			customer.setFirstName(updatedCustomer.getFirstName());
+		}
+		if (customer.getLastName() == null) {
+			customer.setLastName(updatedCustomer.getLastName());
+		}
+		if (customer.getEmail() == null) {
+			customer.setEmail(updatedCustomer.getEmail());
+		}
+		if (customer.getPhone() == null) {
+			customer.setPhone(updatedCustomer.getPhone());
+		}
+		if (customer.getUser() == null) {
+			customer.setUser(updatedCustomer.getUser());
+		}
+		int updateColumns = em.createQuery(jpql).setParameter("id", customer.getId())
+				.setParameter("firstName", customer.getFirstName()).setParameter("lastName", customer.getLastName())
+				.setParameter("email", customer.getEmail()).setParameter("phone", customer.getPhone())
+				.setParameter("user", customer.getUser()).executeUpdate();
+
+		if (updateColumns > 0) {
+			return updatedCustomer;
+		} else {
+
+			return updateCustomer;
+		}
 	}
 
 	@Override
 	public Customer findCustomerById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Customer customer = em.find(Customer.class, id);
+		
+		return customer;
 	}
 
-	
 }
