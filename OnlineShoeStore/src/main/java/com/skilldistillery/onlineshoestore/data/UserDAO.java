095f023aadd5f2f804b6d4c38df4df378f49b933
sazuko07 <sazu07@gmail.com>
2024-12-18 18:38:26 -0600

    userDAOImpl completed blocks

--- OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/UserDAO.java
+++ OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/UserDAO.java
@@ -4,11 +4,7 @@ import com.skilldistillery.jpaonlineshoestore.entities.User;
 
 public interface UserDAO {
 	User addUser(User user);
-<<<<<<< HEAD
-	User deleteUser(User user);
-=======
 	boolean deleteUser(User user);
->>>>>>> Remote_Branch1
 	User updateUser(User user);
 	User findUserById(int id);
 
