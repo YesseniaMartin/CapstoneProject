0804fa4c7c599ba05c9e4af21cd6c125ffa4c60c
yessenia <yesseniaperez.n5@gmail.com>
2024-12-18 18:13:54 -0700

    shoedao

--- OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/ShoeDAO.java
+++ OnlineShoeStore/src/main/java/com/skilldistillery/onlineshoestore/data/ShoeDAO.java
@@ -1,22 +1,13 @@
 package com.skilldistillery.onlineshoestore.data;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> Remote_Branch1
 import com.skilldistillery.jpaonlineshoestore.entities.Shoe;
 
 public interface ShoeDAO {
 	Shoe addShoe(Shoe shoe);
-<<<<<<< HEAD
 	public boolean deleteShoe(Shoe shoe);
 	Shoe updateShoe(Shoe shoe);
 	public List<Shoe> findShoeByKeyword(String keyword);
-=======
-	Shoe deleteShoe(Shoe shoe);
-	Shoe updateShoe(Shoe shoe);
-	Shoe findShoeByKeyword(String keyword);
->>>>>>> Remote_Branch1
 
 }
